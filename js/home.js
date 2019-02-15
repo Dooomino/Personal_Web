19648a50716bf2df157c090b8226ba1732161342
Yudi <yudi.tao@uoit.net>
2019-02-15 10:58:05 -0500

    Add touches gesture

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@ var colors = [
   'rgb(70, 70, 85, 1)'
 ];
 
-var deviceWidth = window.innerWidth;
+var deviceWidth = document.documentElement.clientWidth;
 $('.heads :header').after('<hr class = "heads-line" />');
 
 
@@ -18,6 +18,7 @@ function toggleBars() {
 }
 
 function goto(el) {
+
   id = $(el).attr('href');
   $(".heads-line").each(function (i, el) {
     el.classList.remove("active");
@@ -47,11 +48,75 @@ window.onload = function () {
     $('.heads-text p').each(function (i, el) {
       c = colors.length % (i + 1);
       el.style.backgroundColor = colors[c];
-    });
+    })
+
+    window.addEventListener('mousemove', hoverborder);
+
   }
 }
 
-window.addEventListener('mousemove', function (event) {
+
+window.onresize = function () {
+  deviceWidth = document.documentElement.clientWidth;
+  if (deviceWidth > 960) {
+    $('.heads-text p').each(function (i, el) {
+      c = colors.length % (i + 1);
+      el.style.backgroundColor = colors[c];
+    });
+    window.addEventListener('mousemove', hoverborder);
+  } else {
+    $('.heads-text p').each(function (i, el) {
+      c = colors.length % (i + 1);
+      el.style.backgroundColor = 'white';
+    });
+    window.removeEventListener('mousemove', hoverborder);
+    window.addEventListener('touchstart', Tstart);
+    window.addEventListener('touchmove', Tmove);
+  }
+}
+
+
+var xi, yi;
+
+function Tstart(ent) {
+  t = ent.touches[0];
+
+  xi = t.clientX;
+  yi = t.clientY;
+  console.log(xi, yi);
+}
+
+var Ton = false;
+
+function Tmove(ent) {
+  t = ent.touches[0];
+
+  xn = t.clientX;
+  yn = t.clientY;
+
+  difx = xn - xi;
+  dify = yn - yi;
+  console.log(difx, dify);
+
+  if (difx > 10) {
+    $("#icon-back").addClass("active");
+    $("#left-contain").addClass("active");
+    $("#main-contain").addClass("active");
+    $("#menus").addClass("active");
+  } else if (difx < -10) {
+    $("#icon-back").removeClass("active");
+    $("#left-contain").removeClass("active");
+    $("#main-contain").removeClass("active");
+    $("#menus").removeClass("active");
+  }
+
+}
+
+
+
+
+
+function hoverborder(event) {
   x = event.clientX;
   y = event.clientY;
   deviceWidth = window.innerWidth;
@@ -71,4 +136,4 @@ window.addEventListener('mousemove', function (event) {
       $("#menus").removeClass("active");
     }
   }
-})
+}
