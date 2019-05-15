20d34428a17fd9e39e74ce8335b1fe9d4788018c
Yudi <yudi.tao@uoit.net>
2019-05-15 12:28:56 -0400

    remove redirect port

--- js/preload.js
+++ js/preload.js
@@ -7,7 +7,7 @@ window.onload = function () {
     .then((resp) => resp.text())
     .then(function (data) {
       console.log(data)
-      document.location.href = "http://dooomino.info:16075" + data;
+      document.location.href = "http://dooomino.info" + data;
       //      document.location.href = "http://localhost:16075" + data;
     })
 }
