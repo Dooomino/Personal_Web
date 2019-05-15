94cc00d48985a9b0e492a5ddca6337071421aa22
Yudi <yudi.tao@uoit.net>
2019-05-15 11:45:24 -0400

    Add backend

---
+++ js/preload.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+fetch("http://dooomino.info:16075/lang")
+//  fetch("http://localhost:16075/lang", {
+method: 'GET',
+})
+.then((resp) => resp.text())
+  .then(function (data) {
+    console.log(data)
+    document.location.href = "http://dooomino.info:16075" + data;
+    //      document.location.href = "http://localhost:16075" + data;
+  })
+}
