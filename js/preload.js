83156d2f86cc45709dc7fb75ede8b3bd96c0c161
Yudi <yudi.tao@uoit.net>
2019-05-15 12:30:58 -0400

    =

--- js/preload.js
+++ js/preload.js
@@ -7,7 +7,7 @@ window.onload = function () {
     .then((resp) => resp.text())
     .then(function (data) {
       console.log(data)
-      document.location.href = "http://dooomino.info/index/" + data;
+      document.location.href = "http://dooomino.info/index" + data;
       //      document.location.href = "http://localhost:16075" + data;
     })
 }
