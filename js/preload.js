8be141c47b6eb364309b4c33b0a79ea3c212fafa
Yudi <yudi.tao@uoit.net>
2019-05-16 13:53:15 -0400

    Multiple tries

--- js/preload.js
+++ js/preload.js
@@ -1,13 +1,19 @@
 window.onload = function () {
-  fetch("http://dooomino.info:8090/lang",
-      //  fetch("http://localhost:16075/lang",
-      {
-        method: 'GET',
-      })
-    .then((resp) => resp.text())
-    .then(function (data) {
-      console.log(data)
-      document.location.href = "http://dooomino.info/index" + data;
-      //      document.location.href = "http://localhost:16075" + data;
-    })
+  try {
+    setInterval(() => {
+      fetch("http://dooomino.info:8090/lang",
+          //  fetch("http://localhost:16075/lang",
+          {
+            method: 'GET',
+          })
+        .then((resp) => resp.text())
+        .then(function (data) {
+          console.log(data)
+          document.location.href = "http://dooomino.info/index" + data;
+          //      document.location.href = "http://localhost:16075" + data;
+        })
+    }, 2000);
+  } catch (e) {
+
+  }
 }
