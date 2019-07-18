24ea66536daa316b696992b748e69c2a012c089e
yudi <1744807521@qq.com>
2019-07-18 23:58:14 +0800

    simplified redirect

--- js/preload.js
+++ js/preload.js
@@ -1,19 +1,20 @@
 window.onload = function () {
-  try {
-    setInterval(() => {
-      fetch("http://www.mino.moe:8090/lang",
-          //  fetch("http://localhost:16075/lang",
-          {
-            method: 'GET',
-          })
-        .then((resp) => resp.text())
-        .then(function (data) {
-          console.log(data)
-          document.location.href = "http://www.mino.moe/index" + data;
-          //      document.location.href = "http://localhost:16075" + data;
-        })
-    }, 2000);
-  } catch (e) {
-
-  }
+  document.location = "http://www.mino.moe/index/views/en/index.html"
+  //  try {
+  //    setInterval(() => {
+  //      fetch("http://www.mino.moe:8090/lang",
+  //          //  fetch("http://localhost:16075/lang",
+  //          {
+  //            method: 'GET',
+  //          })
+  //        .then((resp) => resp.text())
+  //        .then(function (data) {
+  //          console.log(data)
+  //          document.location.href = "http://www.mino.moe/index" + data;
+  //          //      document.location.href = "http://localhost:16075" + data;
+  //        })
+  //    }, 2000);
+  //  } catch (e) {
+  //
+  //  }
 }
