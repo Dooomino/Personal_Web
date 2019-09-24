42c12c31d9a4062342a3f58cc423c13afa0aaf3d
Yudi <yudi.tao@uoit.net>
2019-09-24 14:38:26 -0400

    Upgrade to 2.0

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var time = new anime.timeline();
+
+
+function fly() {
+  var rocket = document.getElementById("rocket");
+  var fire = rocket.getElementsByClassName("fire")[0];
+
+  fire.classList.remove("hide");
+  fire.classList.add('wave');
+
+  time.add({
+    targets: rocket,
+    delay: 500,
+    duration: 1000,
+    easing: [.29, -0.3, .67, .9],
+    translateY: ['0%', '-300%'],
+    zIndex: [100, 100],
+    complete: () => {
+      document.location = "http://www.mino.moe"
+    }
+  })
+}
+
+document.getElementById("rocket").onclick = fly;
