c717c05ddc80df3666b2294d0681b64d6054213c
Yudi <yudi.tao@ontariotechu.net>
2020-12-23 02:37:02 -0500

    Add progress for project page

--- pages/js/portfolio.js
+++ pages/js/portfolio.js
@@ -2,3 +2,18 @@ $("#toTop").on("click",function(e){
   e.preventDefault();
   var c = $("html, body").stop().animate({ scrollTop: 0 },1000);
 });
+
+let imageCount = 0;
+let state = 0;
+
+$(window).on('ready', function(e) {
+  $(".progress").addClass("half");
+});
+
+$(window).on('load', function(e) {
+  $(".progress").addClass("ready");
+
+  setTimeout(function (){
+    $(".progress").addClass("end");
+  },2000);
+});
